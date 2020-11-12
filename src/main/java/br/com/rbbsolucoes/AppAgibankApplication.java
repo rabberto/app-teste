17b39ca0e04b507aeee54f7ac93cf3891a46c300
rabberto <rafael.rbberto@gmail.com>
2020-11-12 19:10:06 -0300

    ajustes

--- src/main/java/br/com/rbbsolucoes/AppAgibankApplication.java
+++ src/main/java/br/com/rbbsolucoes/AppAgibankApplication.java
@@ -1,13 +1,26 @@
 package br.com.rbbsolucoes;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class AppAgibankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppAgibankApplication.class, args);
 	}
 
+	@Scheduled(fixedDelay = 5000)
+	public void readFiles() throws IOException {
+		String pathIn = "HOMEPATH/data/in";
+		String pathOut = "HOMEPATH/data/out";
+		ReadFile readFile = new ReadFile();
+		readFile.iniciaLeitura(pathIn, pathOut);
+	}
+	
 }
